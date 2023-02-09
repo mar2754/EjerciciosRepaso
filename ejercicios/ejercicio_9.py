bd161fd96f50cf2e3cea5fb315f9ee1f3be59b94
munoz <munozsanchezmariana0@gmail.com>
2023-02-08 23:22:37 -0500

    Implement exercise 9

--- ejercicios/ejercicio_9.py
+++ ejercicios/ejercicio_9.py
@@ -1,7 +1,13 @@
-print("Ingrese un numero")
-num = int(input())
-fact=1
-for i in range (1, num+1):
-    fact*=i
+def carga_matriz(filas, columnas, vector):
+    for i in range(filas):
+        vector.append([0] * columnas)
+        for j in range(columnas):
+            vector[i][j] = input("Ingrese los elementos: ")
+    print(vector, filas, columnas)
+    # return f, c
 
-    print("El factorial de ",num, "es",fact)
+
+f = int(input("Ingrese la cantidad de filas: "))
+c = int(input("Ingrese la cantidad de columnas: "))
+v = []
+carga_matriz(f, c, v)
