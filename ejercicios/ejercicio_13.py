ccbdf5716609d7928ca76a37bb64d8b3c4e9c650
munoz <munozsanchezmariana0@gmail.com>
2023-02-08 22:38:41 -0500

    Implement exercise 13

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,14 @@
+print("Ingrese número 1:  ")
+n1 = int(input())
+print("Ingrese número 2:  ")
+n2 = int(input())
+
+suma = n1+n2
+resta = n1-n2
+multiplicación = n1*n2
+división = n1/n2
+
+print("La suma es:  ", suma)
+print("La resta es:  ",resta)
+print("La multiplicación es:  ",multiplicación)
+print("La división es:  ",división)
